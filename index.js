99346a84a9b35d79a339ed43308d738500877604
Greg Walker <michael.walker@gsa.gov>
2018-01-24 15:54:03 -0600

    pin the github file header instead of making a custom one

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const updateTextLabel = text => {
-  let div = document.getElementById('___github_current_file');
-  if (!div) {
-    div = document.createElement('div');
-    div.setAttribute('id', '___github_current_file');
-    div.setAttribute('style', 'position: fixed; top: 60px; background: #dbedff; width: 100%; text-align: center; height: 23px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; padding: 0 15px;')
-    document.body.appendChild(div);
-
-    document.querySelector('.pr-toolbar-shadow').setAttribute('style', 'top: 83px;');
-  }
-  div.innerHTML = '<span style="margin-right: 10px;">⤹</span>' + text + '<span style="margin-left: 10px; display: inline-block; transform: scale(-1, 1);">⤹</span>';
-};
-
-const removeTextLabel = () => {
-  let div = document.getElementById('___github_current_file');
-  if (div) {
-    document.body.removeChild(div);
-    document.querySelector('.pr-toolbar-shadow').removeAttribute('style');
-  }
-};
-
-const getCurrentlyScrolledFile = () => {
+const pinCurrentFileHeader = () => {
   const toolbarHeight = document.querySelector('.pr-toolbar').getBoundingClientRect().height;
-  const currentFile = Array.from(document.querySelectorAll('div.file')).filter(e => e.getBoundingClientRect().y < toolbarHeight).pop()
+  let yOffset = toolbarHeight;
+
+  const previousFile = document.querySelector('div.file-header[data-current-pinned-header]');
+  if (previousFile) {
+    yOffset += previousFile.getBoundingClientRect().height;
+    previousFile.removeAttribute('data-current-pinned-header');
+    previousFile.removeAttribute('style');
+  }
+
+  const currentFile = Array.from(document.querySelectorAll('div.file')).filter(e => e.getBoundingClientRect().y < yOffset).pop()
 
   if (currentFile) {
-    return currentFile.querySelector('.file-header').getAttribute('data-path');
+    const header = currentFile.querySelector('.file-header')
+    header.setAttribute('data-current-pinned-header');
+    header.setAttribute('style', 'position: fixed; top: 60px; left: ' + (currentFile.getBoundingClientRect().x) + 'px; width: ' + currentFile.getBoundingClientRect().width + 'px; border: 1px solid #ddd; z-index: 100;');
+    return header.getAttribute('data-path');
   }
 };
 
@@ -32,17 +23,14 @@ let ticking = false;
 
 const handleScrollTick = () => {
   ticking = false;
-  const filename = getCurrentlyScrolledFile();
-  if (filename) {
-    updateTextLabel(filename);
-  } else {
-    removeTextLabel();
-  }
+  pinCurrentFileHeader();
 };
+
+Array.from(document.querySelectorAll('a.btn[aria-label="View the whole file"]')).forEach(button => button.innerText = 'View whole file');
 
 window.addEventListener('scroll', e => {
   if (!ticking) {
     ticking = true;
-    setTimeout(handleScrollTick, 50);
+    setTimeout(handleScrollTick, 100);
   }
 });
